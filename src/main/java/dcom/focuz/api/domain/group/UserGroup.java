fb05e475aca351f346da26b055e7a90c751f8b18
justkode <sobu0715@gmail.com>
2022-07-17 01:25:28 +0900

    feat: UserGroup Entitiy

---
+++ src/main/java/dcom/focuz/api/domain/group/UserGroup.java
@@ -0,0 +1,35 @@
+package dcom.focuz.api.domain.group;
+
+import dcom.focuz.api.domain.user.User;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Builder
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class UserGroup {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "group_id")
+    private Group group;
+
+    @CreatedDate
+    private LocalDateTime joined;
+}
