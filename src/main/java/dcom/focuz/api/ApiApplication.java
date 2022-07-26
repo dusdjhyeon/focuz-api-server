c6817e05177e4ef193cbfa6a90b3be1dae10cff2
justkode <sobu0715@gmail.com>
2022-07-27 01:35:55 +0900

    feat: Add EnableScheduling

--- src/main/java/dcom/focuz/api/ApiApplication.java
+++ src/main/java/dcom/focuz/api/ApiApplication.java
@@ -3,7 +3,9 @@ package dcom.focuz.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 @PropertySource("classpath:/secret.properties")
 public class ApiApplication {
