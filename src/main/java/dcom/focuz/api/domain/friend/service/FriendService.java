df4d725b6cadf156ae480781783ff2c2dd1dc042
dusdjhyeon <dusdj0813@gmail.com>
2022-08-18 22:31:38 +0900

    feat. FriendService save 추가

--- src/main/java/dcom/focuz/api/domain/friend/service/FriendService.java
+++ src/main/java/dcom/focuz/api/domain/friend/service/FriendService.java
@@ -27,7 +27,6 @@ public class FriendService {
     private final FriendRepository friendRepository;
     private final NotificationRepository notificationRepository;
     private final UserService userService;
-    private final NotificationRepository notificationRepository;
 
     @Transactional
     public UserResponseDto.Simple friendRequest(Integer targetUserId) {
@@ -121,6 +120,7 @@ public class FriendService {
             );
 
             friend.setState(FriendState.FRIEND);
+            friendRepository.save(friend);
         }
         else{
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"요청 상태가 아닙니다.");
@@ -232,6 +232,7 @@ public class FriendService {
                 friendRepository.delete(data);
             } else{
                 friend.setState(FriendState.BLOCKED);
+                friendRepository.save(friend);
             }
         }
 
