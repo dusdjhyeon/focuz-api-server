e5182ef337b2f18d31aff16ff00497a44aa40db4
dusdjhyeon <dusdj0813@gmail.com>
2022-08-18 22:32:51 +0900

    feat. TempStudy 틀 완성

--- src/main/java/dcom/focuz/api/domain/study/TempStudy.java
+++ src/main/java/dcom/focuz/api/domain/study/TempStudy.java
@@ -1,21 +1,18 @@
 package dcom.focuz.api.domain.study;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.data.redis.core.RedisHash;
 
 import javax.persistence.Id;
 
+@Builder
 @Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @RedisHash(value = "study", timeToLive = 180)
 public class TempStudy {
     @Id
     private String id;
     private Integer userId;
     private Integer studyTime;
-
-    public TempStudy(Integer userId, Integer studyTime) {
-        this.userId = userId;
-        this.studyTime = studyTime;
-    }
 }
